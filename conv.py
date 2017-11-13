26dbc2b40bea3f1e76ecb4533ba465b21569ad52
Lens Wile Scan <i@lensws.be>
2017-11-14 00:03:13 +0300

    Add files via upload

--- conv.py
+++ conv.py
@@ -10,9 +10,11 @@ def sh_escape(s):
 
 path="/audio/"
 
+#Если файл все еще заливается по шарику samba, то надо подождать
 proc = subprocess.Popen(["lslocks"], stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
 
+#Если файл уже занят кодированием, удалять преждевременно нельзя
 proc = subprocess.Popen(["ps -ela"], stdout=subprocess.PIPE, shell=True)
 (out2, err) = proc.communicate()
 
@@ -22,3 +24,5 @@ if ( (out.find(path)==-1) & (out2.find("ffmpeg")==-1) ):
         os.system("rm "+path+"done/"+sh_escape(file)+".mp3")
         os.system("ffmpeg -i "+path+sh_escape(file)+" -vn -ar 44100 -ac 2 -ab 320k -f mp3 "+path+"done/"+sh_escape(file)+".mp3")
         os.system("rm "+path+sh_escape(file))
+		#для ведения статистики надо php скрипту на сайте передать данные через POST-формат 
+		os.system("curl -d key=48f37ad768d9f72bc01b40fb520b3ff0 -d file="+sh_escape(file)+" http://example.com/stat.php")
